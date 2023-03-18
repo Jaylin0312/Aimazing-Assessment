69c2db3d5ee02f77bf17e0d4e54d066fe78a1d77
Jaylin0312 <68802506+Jaylin0312@users.noreply.github.com>
2023-03-19 03:18:02 +0800

    fix: changed colour of chart

--- vue-app/src/components/Pages/dashboard.vue
+++ vue-app/src/components/Pages/dashboard.vue
@@ -5,31 +5,18 @@ import { dataService } from '../../services/service.js'
 import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
 
-const backgroundColor = [
-    'rgba(255, 99, 132, 0.2)',
-    'rgba(54, 162, 235, 0.2)',
-    'rgba(255, 206, 86, 0.2)',
-    'rgba(75, 192, 192, 0.2)',
-    'rgba(153, 102, 255, 0.2)',
-    'rgba(255, 159, 64, 0.2)',
-]
-
-const borderColor = [
-    'rgba(255,99,132,1)',
-    'rgba(54, 162, 235, 1)',
-    'rgba(255, 206, 86, 1)',
-    'rgba(75, 192, 192, 1)',
-    'rgba(153, 102, 255, 1)',
-    'rgba(255, 159, 64, 1)',
-]
-
 const data = {
     labels: [],
     datasets: [{
         data: [],
         label: 'Your Spending at A Glance',
-        backgroundColor: backgroundColor,
-        borderColor: borderColor,
+        backgroundColor: (ctx) => {
+            const canvas = ctx.chart.ctx;
+            const gradient = canvas.createLinearGradient(0, 0, 0, 500);
+            gradient.addColorStop(0, '#ffa5cc');
+            gradient.addColorStop(1, '#94bbe9');
+            return gradient;
+        },
         borderWidth: 1,
     }],
 }
@@ -80,8 +67,13 @@ export default {
                 datasets: [{
                     data: [],
                     label: 'Your Spending at a glance',
-                    backgroundColor: backgroundColor,
-                    borderColor: borderColor,
+                    backgroundColor: (ctx) => {
+                        const canvas = ctx.chart.ctx;
+                        const gradient = canvas.createLinearGradient(0, 0, 0, 500);
+                        gradient.addColorStop(0, '#ffa5cc');
+                        gradient.addColorStop(1, '#94bbe9');
+                        return gradient;
+                    },
                     borderWidth: 1,
                 }],
             }
